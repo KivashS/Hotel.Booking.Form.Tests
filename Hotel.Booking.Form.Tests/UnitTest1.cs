815c0d1f8e65f75ce9fe876ed8f06e2376b0cbdc
kivas <kivas@DREADN0UGHT>
2022-05-27 17:58:03 +0200

    Add project files.

---
+++ Hotel.Booking.Form.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Hotel.Booking.Form.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
